629caaddd6cdd064787c43011146e5ead12a9e67
Moésio Filho <moesio.f@gmail.com>
2022-08-07 19:40:30 -0300

    fix

--- src/main.py
+++ src/main.py
@@ -39,10 +39,10 @@ def _termination_from_max_fe(max_fe: int) -> typing.Dict:
 def _fn_as_problem(fn) -> typing.Dict:
     return {
         "fit_func": fn,
-        "lb": [0.0] * 13,
-        "ub": [20.0] + [50] * 3 + [1000] + [21.0] * 8,
+        "lb": [0.0] * 9,
+        "ub": [20.0] + [50] * 3 + [1000] + [21.0] * 4,
         "minmax": "min",
-        "n_dims": 13,
+        "n_dims": 9,
         "verbose": True,
         "save_population": False,
         "log_to": "console"
@@ -53,5 +53,7 @@ if __name__ == '__main__':
     pso_model, pso_bs, pso_bf = pso(quadratic_error,
                                     10,
                                     30)
+    print(pso_bs)
+    print(pso_bf)
     pso_model.history.save_global_best_fitness_chart(
         filename="pso_best_fitness")
